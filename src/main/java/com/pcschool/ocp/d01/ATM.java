c930131513608bc6456561d3b8816002dc677df2
MB-study <MB-study@192.168.1.70>
2020-08-11 12:03:47 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d01/ATM.java
@@ -0,0 +1,44 @@
+package com.pcschool.ocp.d01;
+import java.util.Scanner; // 呼叫輸入指令
+
+public class ATM {
+    public static void main(String[] args) {
+        Account account = new Account();
+        run(account);
+    }
+    
+    public static void run(Account account){
+        System.out.println("1.存款");
+        System.out.println("2.提款");
+        System.out.println("3.查詢");
+        System.out.println("9.離開");
+        System.out.println("--------");
+        System.out.print("請選擇: ");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        
+        switch(choice) {
+            case 1:
+                System.out.print("請輸入存款金額: ");
+                int saveMoney = sc.nextInt();
+                account.save(saveMoney);
+                break;
+            case 2:
+                System.out.print("請輸入提款金額: ");
+                int withdrawMoney = sc.nextInt();
+                account.withdraw(withdrawMoney);
+                break;
+            case 3:
+                int balance = account.getBalance();
+                System.out.printf("帳戶餘額: %,d\n", balance);
+                break;
+            case 9:
+                return; // run() 方法結束
+        }
+        System.out.println("按下Enter 鍵繼續....");
+        new Scanner(System.in).nextLine();
+        run(account); // 遞迴
+    }
+    
+    
+}
