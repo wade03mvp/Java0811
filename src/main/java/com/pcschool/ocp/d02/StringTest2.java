198ff60ab4500db9c6e4132fdbf11d82ba7efa5c
MB-study <MB-study@192.168.1.70>
2020-08-12 11:33:56 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d02/StringTest2.java
@@ -0,0 +1,14 @@
+package com.pcschool.ocp.d02;
+
+public class StringTest2 {
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        String s3 = new String("Java"); // s3 在 pool 外面
+        System.out.println(s1 == s2); // String pool 機制: 字串相同共用 pool
+        System.out.println(s1.equals(s2));
+        System.out.println(s1 == s3);
+        s3 = s3.intern(); // s3 放到 pool 裡面
+        System.out.println(s1 == s3);
+    } 
+}
