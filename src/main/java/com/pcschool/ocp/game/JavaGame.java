30a3a1cd89b4c8e57d1017a6795091cf37280ef1
MB-study <MB-study@192.168.1.70>
2020-08-12 11:12:28 +0800

    Java

--- src/main/java/com/pcschool/ocp/game/JavaGame.java
+++ src/main/java/com/pcschool/ocp/game/JavaGame.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class JavaGame {
     public static void main(String[] args) { 
         GameService.start();// 主程式一行結束 => Perfect!
-        // 三層式架構 (重構) => 新增遊戲方便有彈性!
+        // 三層式架構 (重構) => 新增遊戲方便有彈性! 這樣才有 Sence !
     }  
 }
