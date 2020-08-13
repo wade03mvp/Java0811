ae91ed328ae3353618b175483965bf1a25934578
MB-study <MB-study@192.168.1.70>
2020-08-13 11:03:01 +0800

    Java

--- src/main/java/com/pcschool/ocp/game/case3/Poker.java
+++ src/main/java/com/pcschool/ocp/game/case3/Poker.java
@@ -3,12 +3,21 @@ package com.pcschool.ocp.game.case3;
 public class Poker {
     public String mark; // 花色
     public String number; // 號碼
+    public String color; // 顏色
     public int value; // 價值
 
-    public Poker(String mark, String number, int value) {
+    //  建構式
+    public Poker(String mark, String number, String color, int value) {
         this.mark = mark;
         this.number = number;
+        this.color = color;
         this.value = value;
+    }
+
+    //  改寫物件呈現方式
+    @Override
+    public String toString() {
+        return mark + number;
     }
     
     
---
+++ src/main/java/com/pcschool/ocp/game/case3/PokerMain2.java
@@ -0,0 +1,32 @@
+package com.pcschool.ocp.game.case3;
+import java.util.Scanner;
+import java.io.File;
+public class PokerMain2 {
+    public static void main(String[] args) throws Exception { // 拋出例外
+        // 指定檔案位置
+        File f = new File("src\\main\\java\\com\\pcschool\\ocp\\game\\case3\\poker.txt");
+        // 透過 Scanner 抓取檔案內容
+        Scanner sc = new Scanner(f);
+        // 將檔案內容全部
+        String allData = sc.useDelimiter("\\A").next();
+        // 列印
+        System.out.println(allData);
+        // 透過 split() 從 \n 切分資料 
+        String[] rows = allData.split("\n");
+        // 抓第一筆
+        System.out.println(rows[0]);
+        // 分析每一個欄位
+        String[] row = rows[0].split(",");
+        String mark = row[0];
+        String number = row[1];
+        String color = row[2];
+        int value = Integer.parseInt(row[3]);
+        System.out.printf("mark: %s number: %s color: %s value %d\n", mark, number, color, value);
+        // 建立第一個 Poker 物件
+        Poker p1 = new Poker(mark, number, color, value);
+        System.out.println(p1.mark);
+        System.out.println(p1.number);
+        System.out.println(p1.color);
+        System.out.println(p1.value);  
+    }   
+}
