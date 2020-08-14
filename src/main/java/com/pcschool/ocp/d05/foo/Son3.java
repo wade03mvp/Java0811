cb425e4bd9922aacf1f4ed13f013889d2efb4939
MB-study <MB-study@192.168.1.70>
2020-08-14 14:25:12 +0800

    Java

--- src/main/java/com/pcschool/ocp/d05/foo/Son3.java
+++ src/main/java/com/pcschool/ocp/d05/foo/Son3.java
@@ -4,7 +4,7 @@ import com.pcschool.ocp.d05.Father;
 
 public class Son3 {
     public void print() {  
-        // System.out.println(money); // 無法存取
+        // System.out.println(money); // 無法存取 Father
     }
     
 }
