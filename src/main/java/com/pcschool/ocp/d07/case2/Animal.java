edaf3dead395c274690e1fabe5e185215210ccac
MB-study <MB-study@192.168.1.70>
2020-08-18 13:50:49 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d07/case2/Animal.java
@@ -0,0 +1,9 @@
+package com.pcschool.ocp.d07.case2;
+
+public class Animal {
+
+    public void move() {
+        System.out.println("我是 Animal 我會動");
+    }
+
+}
---
+++ src/main/java/com/pcschool/ocp/d07/case2/Fish.java
@@ -0,0 +1,9 @@
+package com.pcschool.ocp.d07.case2;
+
+public class Fish extends Animal {
+
+    public void move() {
+        System.out.println("我是 Fish 我會游泳");
+    }
+
+}
