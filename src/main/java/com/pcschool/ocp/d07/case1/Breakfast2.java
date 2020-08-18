96c960e0da95baf7e30594ddb1de90109d7bafde
MB-study <MB-study@192.168.1.70>
2020-08-18 13:08:23 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d07/case1/Breakfast2.java
@@ -0,0 +1,23 @@
+package com.pcschool.ocp.d07.case1;
+
+import com.pcschool.ocp.d07.case1.Food.Food;
+import com.pcschool.ocp.d07.case1.Food.Hamburger;
+import com.pcschool.ocp.d07.case1.Food.Sandwich;
+import java.util.Arrays;
+
+public class Breakfast2 {
+    public static void main(String[] args) {
+        Food[] foods = {new Sandwich("火腿蛋"), new Sandwich("火腿蛋"), new Hamburger("牛肉蛋堡")}; 
+        // 請問要付多少錢?
+        // Java7
+        int sum = 0;
+        for(Food food : foods) {
+            sum += food.getPrice();
+        }
+        System.out.println(sum);
+        //Java8
+        sum = Arrays.stream(foods).mapToInt(f -> f.getPrice()).sum();
+        System.out.println(sum);
+    }
+    
+}
