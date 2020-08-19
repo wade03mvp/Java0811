01a8bf27be729150e1290d14e11200a7caffdc96
MB-study <MB-study@192.168.1.70>
2020-08-19 09:49:17 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/case2/Pet.java
@@ -0,0 +1,15 @@
+package com.pcschool.ocp.d08.case2;
+
+public interface Pet {
+
+    public static final int legs = 4;
+
+    public abstract void eat();
+
+    public abstract void skill();
+    
+    // Java8
+    default void copyright() {
+        System.out.println("Happy 寵物店");
+    }
+}
