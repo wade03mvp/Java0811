3579660422d269fee541f6f78e4c1c6126ac365f
MB-study <MB-study@192.168.1.70>
2020-08-19 15:45:06 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/Ball.java
@@ -0,0 +1,66 @@
+package com.pcschool.ocp.d08;
+
+import java.util.Objects;
+
+public class Ball {
+    private String color;
+    private int price;
+
+    public Ball(String color, int price) {
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.color);
+        hash = 67 * hash + this.price;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Ball other = (Ball) obj;
+        if (this.price != other.price) {
+            return false;
+        }
+        if (!Objects.equals(this.color, other.color)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Ball{" + "color=" + color + ", price=" + price + '}';
+    }
+    
+}
---
+++ src/main/java/com/pcschool/ocp/d08/case4/School2.java
@@ -0,0 +1,38 @@
+package com.pcschool.ocp.d08.case4;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.stream.Stream;
+
+public class School2 {
+    
+    public static void main(String[] args) {
+        Person[] p1 = {
+            new Teacher("T1", 30, 23.3, 70000),
+            new Teacher("T2", 35, 25.4, 90000),
+            new Student("S1", 16, 17.5),
+            new Student("S2", 17, 22.5),
+            new Student("S3", 18, 24.5),
+        };
+        Person[] p2 = {
+            new Teacher("T3", 30, 23.3, 70000),
+            new Teacher("T4", 35, 25.4, 90000),
+            new Teacher("T5", 32, 26.4, 80000),
+            new Student("S4", 19, 21.5),
+            new Student("S5", 20, 18.5),
+        };
+        Person[][] persons = {p1, p2};
+        
+        // 求 Teacher 的平均薪資 ?
+        
+        double salaryAvg = Arrays.stream(persons)
+                .flatMap(Stream::of)    
+                .filter(p -> p instanceof Teacher)
+                //.peek(System.out::println) 印出轉換後的資料,做觀察
+                .map(p -> (Teacher)p)
+                .mapToInt(t -> t.getSalary().getMoney())
+                .average()
+                .getAsDouble();
+        System.out.println(salaryAvg);
+    }
+}
