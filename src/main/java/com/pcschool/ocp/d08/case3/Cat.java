980806e59c4bc3e61a8d35cef11b40f753aba6aa
MB-study <MB-study@192.168.1.70>
2020-08-19 10:09:56 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/case3/Cat.java
@@ -0,0 +1,8 @@
+package com.pcschool.ocp.d08.case3;
+
+public abstract class Cat implements Pet {
+
+    public void eat() {
+        System.out.println("魚");
+    }
+}
---
+++ src/main/java/com/pcschool/ocp/d08/case3/Dog.java
@@ -0,0 +1,8 @@
+package com.pcschool.ocp.d08.case3;
+
+public abstract class Dog implements Pet {
+
+    public void eat() {
+        System.out.println("骨頭");
+    }
+}
