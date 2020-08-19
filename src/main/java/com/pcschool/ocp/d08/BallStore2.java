db7cec34cd25d2b9e93b4ba19b20448dc1f93b1c
MB-study <MB-study@192.168.1.70>
2020-08-19 15:59:19 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/BallStore2.java
@@ -0,0 +1,19 @@
+package com.pcschool.ocp.d08;
+
+import com.pcschool.ocp.d08.*;
+import java.util.Arrays;
+
+public class BallStore2 {
+    public static void main(String[] args) {
+        final Ball target = new Ball("Red", 100);
+        Ball[] balls = {
+            new Ball("Red", 100),
+            new Ball("Blue", 100),
+            new Ball("Red", 100),
+            new Ball("Yellow", 300),
+            new Ball("Red", 200),
+        };
+        // 請將符合 target 的 Ball 列出 :
+        Arrays.stream(balls).filter(b -> b.equals(target)).forEach(System.out::println);
+    }
+}
