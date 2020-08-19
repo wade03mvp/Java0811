967b9731aeb64ccda99360964fd2d5be1c63c55a
MB-study <MB-study@192.168.1.70>
2020-08-19 11:58:52 +0800

    Java

--- src/main/java/com/pcschool/ocp/d08/army/ArmyMain.java
+++ src/main/java/com/pcschool/ocp/d08/army/ArmyMain.java
@@ -1,5 +1,7 @@
 package com.pcschool.ocp.d08.army;
 
+import java.util.Arrays;
+
 public class ArmyMain {
     
     public static void main(String[] args) {
@@ -7,6 +9,7 @@ public class ArmyMain {
         Airplane[] airplanes = {new Fighter(), new Fighter(), new Fighter()};
         Car[] cars = {new Tank(), new Tank(), new Truck(), new Truck()};
         Weapon[] weapons = {new Fighter(), new Tank()};
+        System.out.println("------------");
         
         // cars 要統一發射武器
         for(Car car : cars) {
@@ -19,5 +22,14 @@ public class ArmyMain {
         for(Weapon w : weapons) {
             w.shoot();
         }
+        System.out.println("------------");
+        
+        // Java8 : cars 要統一發射武器
+        Arrays.stream(cars)
+                .filter(c -> c instanceof Weapon) // 過濾出 cars 包含 Weapon 的元素 (還是 Car 型別)
+                .map(c -> (Weapon)c)   // cars 轉型成 Weapon
+                .forEach(c -> c.shoot());
+        // Java8 : weapons 要統一發射武器
+        Arrays.stream(weapons).forEach(w -> w.shoot());           
     }
 }
