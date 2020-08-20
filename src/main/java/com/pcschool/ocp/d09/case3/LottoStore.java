c80aa0ffb5d9ddfdea92ded814ff1bdc13f8f27b
MB-study <MB-study@192.168.1.70>
2020-08-20 10:47:45 +0800

    Java

--- src/main/java/com/pcschool/ocp/d09/case3/LottoStore.java
+++ src/main/java/com/pcschool/ocp/d09/case3/LottoStore.java
@@ -4,12 +4,12 @@ import java.util.Random;
 
 public class LottoStore {
     public static void main(String[] args) {
-        Lotto one = new Lotto() {          // 匿名內部類別
+        Lotto one = new Lotto() {  // 匿名內部類別(開始)
             @Override
             public void printNumber() {
                 System.out.printf("%d\n", new Random().nextInt(10));
             }
-        };
+        };  // 匿名內部類別(結束)
         one.printNumber();
         
         // Lambda 語法 (不用選實作方法)
