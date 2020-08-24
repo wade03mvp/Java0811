17e7b40111e001d6670a097ecadbf8ce55247f68
MB-study <MB-study@192.168.1.70>
2020-08-24 14:28:28 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d12_exception/case3/InputNumberAndCalc.java
@@ -0,0 +1,35 @@
+package com.pcschool.ocp.d12_exception.case3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputNumberAndCalc {
+    public static void main(String[] args) {
+        input();
+    }
+    
+    public static void input() {
+        System.out.printf("請輸入分母: ");
+        Scanner sc = new Scanner(System.in);
+        try {
+            int d = sc.nextInt();
+            int result = calc(d);
+            print(result);
+        } catch (InputMismatchException e) {
+            System.out.println("輸入錯誤, 請重新輸入~");
+            input();
+        } catch (ArithmeticException e2) {
+            System.out.println("發生數學錯誤, 分母不可為0, 請重新輸入 !");
+            input();
+        }    
+    }
+    
+    public static int calc(int denominator) { // denominator 分母
+        int result = 100 / denominator;
+        return result;
+    }
+    
+    public static void print(int result) {
+        System.out.printf("result = %d\n", result);
+    }
+}
---
+++ src/main/java/com/pcschool/ocp/d12_exception/case4/UncheckedAndChecked.java
@@ -0,0 +1,60 @@
+package com.pcschool.ocp.d12_exception.case4;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UncheckedAndChecked {
+    public static void main(String[] args) {
+        a(0);
+        b(0);
+        
+        try {
+            c(0);
+        } catch (ArithmeticException e) {
+            System.out.printf("main 自行處理數學錯誤: %s\n", e);
+        }
+        
+        try {
+            d(0);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public static void a(int d) {
+       int x = 10;
+        try {
+            int result = x / d;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("分母不可為 0");
+        }
+    }
+    
+    public static void b(int d) {
+        int x = 10;
+        if(d == 0) {
+            System.out.println("分母不可為 0");
+            return;
+        }
+        int result = x / d;
+        System.out.println(result);
+    }
+    
+    public static void c(int d) {
+        int x = 10;
+        int result = x / d;
+        System.out.println(result);
+    }
+    
+    public static void d(int d) throws Exception{
+        int x = 10;
+        if(d == 0) {
+            // 自己建立一個錯誤物件
+            Exception e = new Exception("d 說: 分母不可為 0");
+            throw e;
+        }
+        int result = x / d;
+        System.out.println(result);
+    }
+}
